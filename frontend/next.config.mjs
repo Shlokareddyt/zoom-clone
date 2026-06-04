3688826f26fb62a45e2b888a67ee17a23f47e6cf
Shlokareddyt <185192903+Shlokareddyt@users.noreply.github.com>
2026-06-04 23:58:59 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    outputFileTracingIncludes: {
-      '/**': ['./app/**/*'],
-    },
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
